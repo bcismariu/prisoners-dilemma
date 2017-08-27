4e9a4cbd3481a9b976d0acc5f1076e683413c4b7
Andronic Tudor <atudorel12@yahoo.com>
2017-08-27 20:00:32 +0300

    Add algorithm in web page

--- strategies/Tudor.js
+++ strategies/Tudor.js
@@ -3,19 +3,22 @@ function Tudor()
 	this.results = [];	
 	this.ok = 0;
 	this.contor = 0;
+	this.rounds_collaborate = 2;
+	this.rounds_betray = 3;
+	
 	this.answer = function()
 	{
-		this.rounds_collaborate = 2;
-		this.rounds_betray = 3
 		if(ok == 0)
-			return collaborate(rounds_collaborate);
+			return this.collaborate(rounds_collaborate);
 		if(ok == 1)
-			return betray(rounds_betray);
+			return this.betray(rounds_betray);
 	}
+	
 	this.lastRestul = function (result)
 	{
 		this.results.push(result);
 	}
+	
 	this.collaborate = function(rounds)
 	{
 		if(contor == rounds)
@@ -30,6 +33,7 @@ function Tudor()
 			return 'C';
 		}
 	}
+	
 	this.betray = function (rounds)
 	{
 		if(contor == rounds)
